4e859598839e3592f3cf55fa91ead6b24df83abc
Isaac Denny <isaacquinndenny@gmail.com>
2024-11-19 16:01:46 +0000

    server ip

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP "127.0.0.1" // The server's IP address (use Raspberry Pi server IP in actual setup)
+#define SERVER_IP "10.33.96.43" // The server's IP address (use Raspberry Pi server IP in actual setup)
 #define SERVER_PORT 8080      // Port the server is listening on
 #define BUFFER_SIZE 1024      // Buffer size for messages
 
